7541d8debb29286aa614df9c6b57d3bc2f2f9b0e
doyeon <kimsk3113@naver.com>
2022-12-26 18:55:13 +0900

    chore: 프로젝트 환경 설정

---
+++ src/test/java/com/unicornstudy/singleshop/SingleShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unicornstudy.singleshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SingleShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
