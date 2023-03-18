663e07527d73224f94d2677ccef7465b79773502
we4sley <galaxy138742@gmail.com>
2023-03-18 11:13:56 +0900

    refactor: 장바구니 생성 시 cartItems 리스트에 cartItem 추가

--- src/main/java/com/unicornstudy/singleshop/carts/Cart.java
+++ src/main/java/com/unicornstudy/singleshop/carts/Cart.java
@@ -56,6 +56,7 @@ public class Cart {
 
     private void initializeCartItems(CartItem cartItem) {
         cartItem.initializeCart(this);
+        this.cartItems.add(cartItem);
         this.price = cartItem.getItem().getPrice();
         this.count = 1;
     }
